59a54aa1c03c6387598ff6311c836c36c14c5999
Cthultist <Valkyric_Death@hotmail.com>
2022-07-16 22:46:04 -0700

    beginning to piece together

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::collections::HashMap;
+struct Todo {
+    //Use the built-in HashMap of trust to store key value pairs
+    map: HashMap<String, bool>,
+}
+impl Todo {
+    fn inser(&mut self, key: String) {
+        self.map.insert(key, true);
+    }
+}
+
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action.");
     let item = std::env::args().nth(2).expect("Please specify an item.");
